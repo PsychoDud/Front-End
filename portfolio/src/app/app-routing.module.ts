a679c782080bdcd95a33811c71fd82af28b7c86a
PsychoDud <emanuelstaffa@gmail.com>
2022-08-29 20:15:38 -0300

    version final

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditeducacionComponent } from './partes/educacion/editeducacion.component';
+import { NeweducacionComponent } from './partes/educacion/neweducacion.component';
+import { EditExperienciaComponent } from './partes/experiencia/edit-experiencia.component';
+import { NewExperienciaComponent } from './partes/experiencia/new-experiencia.component';
+import { HomeComponent } from './partes/home/home.component';
+import { LoginComponent } from './partes/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'nuevaexp', component: NewExperienciaComponent},
+  { path: 'editexp/:id', component: EditExperienciaComponent},
+  { path: 'nuevaedu', component: NeweducacionComponent},
+  { path: 'editedu/:id', component: EditeducacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,42 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PartesComponent } from './partes/partes.component';
-import { BannerComponent } from './partes/banner/banner.component';
-import { EducacionComponent } from './partes/educacion/educacion.component';
-import { ExperienciaComponent } from './partes/experiencia/experiencia.component';
-import { FooterComponent } from './partes/footer/footer.component';
 import { HeaderComponent } from './partes/header/header.component';
+import { LogoAPComponent } from './partes/logo-ap/logo-ap.component';
+import { BannerComponent } from './partes/banner/banner.component';
+import { SobreMiComponent } from './partes/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './partes/experiencia/experiencia.component';
+import { EducacionComponent } from './partes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './partes/skills/skills.component';
+import { ProyectosComponent } from './partes/proyectos/proyectos.component';
+import { FooterComponent } from './partes/footer/footer.component';
+import {HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './partes/home/home.component';
 import { LoginComponent } from './partes/login/login.component';
-import { LogoApComponent } from './partes/logo-ap/logo-ap.component';
-import { ProyectosComponent } from './partes/proyectos/proyectos.component';
-import { SkillsComponent } from './partes/skills/skills.component';
-import { SobreMiComponent } from './partes/sobre-mi/sobre-mi.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './partes/experiencia/new-experiencia.component';
+import { FormsModule } from '@angular/forms';
+import { EditExperienciaComponent } from './partes/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './partes/educacion/neweducacion.component';
+import { EditeducacionComponent } from './partes/educacion/editeducacion.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PartesComponent,
-    BannerComponent,
-    EducacionComponent,
-    ExperienciaComponent,
-    FooterComponent,
     HeaderComponent,
+    LogoAPComponent,
+    BannerComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent,
     HomeComponent,
     LoginComponent,
-    LogoApComponent,
-    ProyectosComponent,
-    SkillsComponent,
-    SobreMiComponent
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
+    FormsModule,
+    
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
