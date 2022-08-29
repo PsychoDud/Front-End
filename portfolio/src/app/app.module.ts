11b1b2499396fb2badcd53a04ef5b028b2df5a31
PsychoDud <emanuelstaffa@gmail.com>
2022-08-29 19:07:16 -0300

    fc

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PartesComponent } from './partes/partes.component';
+import { BannerComponent } from './partes/banner/banner.component';
+import { EducacionComponent } from './partes/educacion/educacion.component';
+import { ExperienciaComponent } from './partes/experiencia/experiencia.component';
+import { FooterComponent } from './partes/footer/footer.component';
+import { HeaderComponent } from './partes/header/header.component';
+import { HomeComponent } from './partes/home/home.component';
+import { LoginComponent } from './partes/login/login.component';
+import { LogoApComponent } from './partes/logo-ap/logo-ap.component';
+import { ProyectosComponent } from './partes/proyectos/proyectos.component';
+import { SkillsComponent } from './partes/skills/skills.component';
+import { SobreMiComponent } from './partes/sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartesComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    LogoApComponent,
+    ProyectosComponent,
+    SkillsComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
